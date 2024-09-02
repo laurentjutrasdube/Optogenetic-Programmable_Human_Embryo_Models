082fe21e1a5a61075e783adf2a269b65d18c16a3
Laurent Jutras-Dubé <35509198+laurentjutrasdube@users.noreply.github.com>
2024-09-02 19:00:11 -0400

    Update codes

--- OptoBMP4_functions.py
+++ OptoBMP4_functions.py
@@ -8,11 +8,38 @@ from matplotlib.animation import (FuncAnimation, writers)
 # 1. Partial differential equations (PDEs)
 #    modelling the gene regulatory network
 
+def compute_edge_parameters(x, param):
+
+    x_total = param["x_total"]
+    x_light = param["x_light"]
+    K1 = param["K_pSMAD1"]
+
+    if param["micro"]: 
+
+        # Impact of tension on WNT3 production
+        r = x-x_total+205.
+        r[r<0.] = 0.
+        T = param["T_WNT3"]*((r/160.)**35./(1.+(r/160.)**35.)+1.)
+
+        # Sensitivity to BMP4
+        K = K1/(1.+(r/160.)**35.)+0.1
+        if param["fixed_BMP4"] == 0.:
+            k2 = K1*(x**25./((x_light*1.5)**25.+x**25.))+0.1
+            K = np.min(np.array([K, k2]), axis=0)
+            
+    else:
+        T = param["T_WNT3"]
+        K = K1*(np.abs(x)**25./((x_light*1.5)**25.+np.abs(x)**25.))+0.1
+
+    return K, T
+
+
+
 def PDEs(t, var, param):
     
     # Distance from the center
     x_total, x_arr_len, x_light = param["x_total"], param["x_arr_len"], param["x_light"]
-    x = np.abs(np.linspace(-x_total, x_total, x_arr_len))
+    x = np.linspace(-x_total, x_total, x_arr_len)
     if param["micro"]:   x = np.linspace(0., x_total, x_arr_len)
     
     # Variables
@@ -24,36 +51,21 @@ def PDEs(t, var, param):
     free_BMP4 = BMP4/(1.+param["a"]*NOG)
 
     # Parameters
-    n1, K1 = param["n_pSMAD1"], param["K_pSMAD1"]
+    n1 = param["n_pSMAD1"]
     n2, K2 = param["n_nYAP"], param["K_nYAP"]
     n3, K3 = param["n_bCAT"], param["K_bCAT"]
 
-    # Edge-depenedent parameters
-    if param["micro"]: 
-
-        # Impact of tension on WNT3 production
-        r = x-x_total+205.
-        r[r<0.] = 0.
-        T = param["T_WNT3"]*((r/160.)**35./(1.+(r/160.)**35.)+1.)
-
-        # Sensitivity to BMP4
-        K = K1/(1.+(r/160.)**35.)+0.5
-        if param["fixed_BMP4"] == 0.:
-            k2 = K1*(x**25./((x_light*1.5)**25.+x**25.))+0.1
-            K = np.min(np.array([K, k2]), axis=0)
-            
-    else:
-        T = param["T_WNT3"]
-        K = K1*(x**25./((x_light*1.5)**25.+x**25.))+0.1
+    # Edge-dependent parameters
+    K1, T = compute_edge_parameters(x, param)
     
     # pSMAD1, nYAP & nß-CATENIN
-    pSMAD1 = free_BMP4**n1/(free_BMP4**n1+K**n1)
+    pSMAD1 = free_BMP4**n1/(free_BMP4**n1+K1**n1)
     nYAP = pSMAD1**n2/(pSMAD1**n2+K2**n2)
     if param["fixed_nYAP"] != 0.:    nYAP = param["fixed_nYAP"]
     bCAT = WNT3**n3/(WNT3**n3+K3**n3)
     
     # Production terms
-    prod_BMP4 = param["p_BMP4"]*np.heaviside(x_light-x, 1)
+    prod_BMP4 = param["p_BMP4"]*np.heaviside(x_light-np.abs(x), 1)
     prod_NOG = param["p_NOG"]*pSMAD1
     prod_WNT3 = (param["p1_WNT3"]*pSMAD1 +param["p2_WNT3"]*bCAT) *(1.-nYAP) *T
     prod_NODAL = param["p_NODAL"]*bCAT *(1.-nYAP)
@@ -94,6 +106,68 @@ def PDEs(t, var, param):
 
 
 # 2. Integration routine
+
+def get_results(param):
+
+    # Position along the micropattern
+    x = np.linspace(-param["x_total"], param["x_total"], param["x_arr_len"])
+    if param["micro"]:    x = np.linspace(0., param["x_total"], param["x_arr_len"])
+    
+    # BMP4, NOGGIN, WNT3 and NODAL concentrations
+    results = integrate(param)
+    BMP4 = results[:,:param["x_arr_len"]]
+    NOG = results[:,param["x_arr_len"]:2*param["x_arr_len"]]
+    WNT3 = results[:,2*param["x_arr_len"]:3*param["x_arr_len"]]
+    NODAL = results[:,3*param["x_arr_len"]:]
+    
+    # Sensitivity to BMP4 (K parameter)
+    K_pSMAD1, T = compute_edge_parameters(x, param)
+    
+    # Free BMP4
+    free_BMP4 = BMP4/(1.+param["a"]*NOG)
+    
+    # pSMAD1
+    n_pSMAD1 = param["n_pSMAD1"]
+    pSMAD1 = free_BMP4**n_pSMAD1/(free_BMP4**n_pSMAD1+K_pSMAD1**n_pSMAD1)
+    
+    # nYAP
+    n_nYAP, K_nYAP = param["n_nYAP"], param["K_nYAP"]
+    nYAP = pSMAD1**n_nYAP/(pSMAD1**n_nYAP+K_nYAP**n_nYAP)
+    
+    # nß-CATENIN
+    n_bCAT, K_bCAT = param["n_bCAT"], param["K_bCAT"]
+    bCAT = WNT3**n_bCAT/(WNT3**n_bCAT+K_bCAT**n_bCAT)
+    
+    # pSMAD2
+    n_pSMAD2, K_pSMAD2 = param["n_pSMAD2"], param["K_pSMAD2"]
+    pSMAD2 = NODAL**n_pSMAD2/(NODAL**n_pSMAD2+K_pSMAD2**n_pSMAD2)
+    
+    # ISL1
+    n_ISL1, K_ISL1 = param["n_ISL1"], param["K_ISL1"]
+    ISL1 = pSMAD1**n_ISL1/(pSMAD1**n_ISL1+K_ISL1**n_ISL1)
+    
+    # BRA
+    n_BRA, K_BRA = param["n_BRA"], param["K_BRA"]
+    n2_BRA, K2_BRA = param["n2_BRA"], param["K2_BRA"]
+    n3_BRA, K3_BRA = param["n3_BRA"], param["K3_BRA"]
+    BRA = pSMAD2**n_BRA/(pSMAD2**n_BRA+K_BRA**n_BRA)
+    BRA *= K2_BRA**n2_BRA/(ISL1**n2_BRA+K2_BRA**n2_BRA)
+    BRA *= bCAT**n3_BRA/(bCAT**n3_BRA+K3_BRA**n3_BRA)
+    
+    # SOX2
+    n_SOX2, K_SOX2 = param["n_SOX2"], param["K_SOX2"]
+    n2_SOX2, K2_SOX2 = param["n2_SOX2"], param["K2_SOX2"]
+    SOX2 = K_SOX2**n_SOX2/(pSMAD1**n_SOX2+K_SOX2**n_SOX2)
+    SOX2 *= K2_SOX2**n2_SOX2/(BRA**n2_SOX2+K2_SOX2**n2_SOX2)
+    
+    # Grouping results
+    morphogens = np.array([BMP4, free_BMP4, NOG, WNT3, NODAL])
+    TFs = np.array([pSMAD1, nYAP, bCAT, pSMAD2])
+    fates = np.array([SOX2, ISL1, BRA])
+
+    return x, morphogens, TFs, fates
+
+
 
 def integrate(param):
     
